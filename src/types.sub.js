4e94b1136f3e170b4744e7c59a89c82854682c04
Jan Andrle <andrle.jan@centrum.cz>
2020-12-09 17:18:44 +0100

    Update types.sub.js

--- src/types.sub.js
+++ src/types.sub.js
@@ -3,7 +3,8 @@
  * @typedef {String} nodeName
  */
 /**
- * Is a [DOMString](https://developer.mozilla.org/en-US/docs/Web/API/DOMString) representing the URL of an external resource. It reflects the [`src`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script#attr-src) attribute (similary for `href`).
+ * Is a [DOMString](https://developer.mozilla.org/en-US/docs/Web/API/DOMString) representing the URL of an external resource.
+ * It reflects the [`src`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script#attr-src) attribute (similary for `href`).
  * @typedef {String} url
  */
 /**
