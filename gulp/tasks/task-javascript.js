ec2b28722de65e808c84c9e168fbc777ecf3296a
Jan Andrle <andrle.jan@centrum.cz>
2020-12-10 14:47:56 +0100

    Update task-javascript.js

--- gulp/tasks/task-javascript.js
+++ gulp/tasks/task-javascript.js
@@ -1,8 +1,8 @@
 /* jshint esversion: 6,-W097, -W040, node: true, expr: true */
 /*jsondoc={
-    "version": "0.1.3",
-    "script_name": "gulp_task_javascript",
-    "description": "Gulp task for processing `*.js` in `js/` subdirectory inside `app.directories.src`. More description __TBD__.",
+    "version": "0.1.4",
+    "script_name": "gulp_task_javascript_lib",
+    "description": "Gulp task for processing `*.js` in `app.directories.src`. It also validate non `*.min.js` output files (in `app.directories.bin`). More description __TBD__."
     "root_path": "gulp_tasks"
 }*/
 module.exports= function({gulp, scripts, $g, $o, app, cordova_target_device, error}){
@@ -10,7 +10,6 @@ module.exports= function({gulp, scripts, $g, $o, app, cordova_target_device, err
         [ folder, files_pattern, files_not_pattern ]= [ app.directories.src, "*.js", "*.sub.js" ],
         destination= app.directories.bin;
     const [jshint_cmd, ...jshint_rest]= scripts.jshint.split(" ");
-    const skip_final_jshint= [ "module", "namespace", "bundle" ].map(tag=> `${app.name}${tag?"-":""}${tag}.min.js`);
     /* jshint -W061 */const gulp_place= $g.place({ variable_eval: (str)=> eval(str), filesCleaner: require("../gulp_cleanJSHINT.js") });/* jshint +W061 */
     return function(cb){
         let out_files= [];
@@ -23,21 +22,17 @@ module.exports= function({gulp, scripts, $g, $o, app, cordova_target_device, err
     
         function run(){
             return new Promise(function(resolve){
-                let main_stream= gulp.src([ `${folder}${files_pattern}`, `!${folder}${files_not_pattern}` ])
-                        .pipe(gulp_place({ folder, string_wrapper: '"' }))
-                        .pipe($g.replace(/\/\/gulp\.remove\.line\r?\n/g, ""))
-                        .pipe($g.replace(/[^\n]*(\/\*\s*gulp\s\*\/)?\/\*\s*global gulp_place\s*\*\/\r?\n/g,""));
-        
-                main_stream= main_stream.pipe($g.minify_js({ ext: { min: ".min.js" }, noSource: false, mangle: true, compress: { conditionals: true, evaluate: true } }));
-        
-                main_stream
+                gulp.src([ `${folder}${files_pattern}`, `!${folder}${files_not_pattern}` ])
+                    .pipe(gulp_place({ folder, string_wrapper: '"' }))
+                    .pipe($g.replace(/\/\/gulp\.remove\.line\r?\n/g, ""))
+                    .pipe($g.replace(/[^\n]*(\/\*\s*gulp\s\*\/)?\/\*\s*global gulp_place\s*\*\/\r?\n/g,""))
+                    .pipe($g.minify_js({ ext: { min: ".min.js" }, noSource: false, mangle: true, compress: { conditionals: true, evaluate: true } }))
                     .on('error', error.handler)
-                    .on('data', function({ basename }= {}){ if(skip_final_jshint.indexOf(basename)===-1) out_files.push(basename); })
+                    .on('data', function({ basename }= {}){ if(!/.*\.min.js$/.test(basename)) out_files.push(basename); })
                     .pipe(gulp.dest(destination))
                     .on('end', resolve);
             });
         }
-    
         function jsHint(files_for_lint){
             return new Promise(function(resolve, reject){
                 let cmd= $o.spawn(jshint_cmd, [...jshint_rest, files_for_lint], {});
